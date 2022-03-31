dcd198001aeeb0a5a647bdfe38b29531ce724986
William Botbyl <billybotbyl81@yahoo.com>
2022-03-31 17:55:38 -0400

    fixed spelling

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -5,7 +5,7 @@ function GameOver() {
   const { gameOver, correctWord, currAttempt } = useContext(AppContext);
   return (
     <div className="gameOver">
-      <h3>{gameOver.guessedWord ? "You guessed correctly" : "Wron choice"}</h3>
+      <h3>{gameOver.guessedWord ? "You guessed correctly" : "Wrong choice"}</h3>
       <h1>Correct: {correctWord}</h1>
       {gameOver.guessedWord && (
         <h3>You guessed in {currAttempt.attempt} attempts </h3>
